847fa949cd9b85f4fb6f67566621fd5451d5c296
Nicolas Casanova <nicasanovac@gmail.com>
2019-10-05 10:19:45 -0500

    agregado cinemachine, MainScene y un static EventManager
    EvenManager esta en el Jam namespace

---
+++ JAM/Assets/Scripts/MainScene/EventManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Jam { 
+public  class EventManager
+{
+    public static System.Action evento1;
+    
+}
+}
