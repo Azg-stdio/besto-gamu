82a84183e86956a1ce038a8d3661c975f37b9473
oscar1992 <xirious@hotmail.com>
2019-10-04 21:48:14 -0500

    Commit inicial
    Commit inicial

---
+++ JAM/Assets/Scripts/Restart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public float total;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        total -= Time.deltaTime;
+        if (total < 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
