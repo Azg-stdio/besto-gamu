9e84edc0fba36abde93e89a08b0f4159f3a0305d
oscar1992 <xirious@hotmail.com>
2019-10-04 23:50:31 -0500

    Se agrega movimiento 3d
    Se agrega el movimiento en tercera persona

---
+++ JAM/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float multiplicadorRotacion;
+    public float multiplicadorMovimiento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(0, 0, v*multiplicadorMovimiento));
+        transform.transform.Rotate(0, h*multiplicadorRotacion, 0, Space.Self);
+    }
+}
