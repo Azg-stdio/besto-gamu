020ee811c923c9aa9ac428000a71d539b0a3a301
oscar1992 <xirious@hotmail.com>
2019-10-04 22:10:02 -0500

    Timer
    Se agrega el timer a 30 seg y reinicia la escena

--- JAM/Assets/Scripts/Restart.cs
+++ JAM/Assets/Scripts/Restart.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Restart : MonoBehaviour
 {
     public float total;
+    public Text timerF;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,7 @@ public class Restart : MonoBehaviour
     void Update()
     {
         total -= Time.deltaTime;
+        timerF.text = (int) total + "";
         if (total < 0)
         {
             Application.LoadLevel(Application.loadedLevel);
