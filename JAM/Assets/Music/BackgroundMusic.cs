ce9974abdde0cd56be0f454c971e746d8af5aaee
Miguel Arbeláez <miguel_arbelaez@users.noreply.github.com>
2019-10-06 16:04:48 -0500

    Music

---
+++ JAM/Assets/Music/BackgroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioSource intro, loop;
+
+    void Start()
+    {
+        StartCoroutine(IntroPlay());
+    }
+
+    private IEnumerator IntroPlay()
+    {
+        intro.Play();
+        yield return new WaitForSeconds(5.05f);
+        loop.Play();
+        intro.Stop();        
+    }
+}
