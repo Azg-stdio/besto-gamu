abf3e3f164bc3c51f09bf9a368a2391c7d53041e
Nicasanovac <nicasanovac.aux@gmail.com>
2019-10-05 22:30:40 -0500

    added player prefab to SampleScene

---
+++ JAM/Assets/Scripts/Text/TextPopUpController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextPopUpController : MonoBehaviour
+{
+    [SerializeField] TextMeshPro textMesh = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
